2cfafe2c02925c593121e6fa58f1a71e70068189
patrikx3 <alabard@gmail.com>
2021-11-10 09:26:49 +0100

    r0b08x [chore]   11/10/2021, 9:26:49 AM

--- src/replaces.js
+++ src/replaces.js
@@ -183,6 +183,8 @@ ${angularPkg.version}
     build += opencollectiveHeader
 
     const domainInfo = `
+# Warning
+2021. November 17. - All patrikx3.com, corifeus.com and sub-domains are offline. There is electricity upgrade. Will come back again on 2021. November 18.
 
 **Bugs are evident™ - MATRIX️**
     `
